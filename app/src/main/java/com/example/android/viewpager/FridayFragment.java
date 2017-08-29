b4d69fb96e04017e89e84a8b99085d0523602a14
Tatchagon Koonkoei <o_k_t@hotmail.com>
2017-08-29 17:25:38 +0700

    Add Thursday Fragment and Friday Fragment

---
+++ app/src/main/java/com/example/android/viewpager/FridayFragment.java
@@ -0,0 +1,21 @@
+package com.example.android.viewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Fragment that displays "Friday"
+ */
+public class FridayFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friday, container, false);
+    }
+
+}
--- app/src/main/java/com/example/android/viewpager/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/viewpager/SimpleFragmentPagerAdapter.java
@@ -32,15 +32,19 @@ public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         if (position == 0) {
             return new MondayFragment();
-        } else if (position == 1){
+        } else if (position == 1) {
             return new TuesdayFragment();
-        } else {
+        } else if (position == 2) {
             return new WednesdayFragment();
+        } else if (position == 3) {
+            return new ThursdayFragment();
+        } else {
+            return new FridayFragment();
         }
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return 5;
     }
 }
